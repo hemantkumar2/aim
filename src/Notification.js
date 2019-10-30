5e9d8328f45ddd6c8911e9618ab5ccf7dcd95a17
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-10-30 23:39:29 +0530

    Notifications added

--- src/Notification.js
+++ src/Notification.js
@@ -7,6 +7,34 @@ const Notification = () => {
     <div>
       <Jumbotron />
       <h1>Notification</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card-body text-left">
+                <p>
+                  1. New class for SSC starting from 12-11-2019.
+                </p>
+                <p>
+                  2. New class for IBPS Clerk starting from 09-11-2019.
+                </p>
+                <p>
+                  3. Batch of 23 AUG will be merged with 16 july batch.
+                </p>
+                <p>
+                  4. Aspirants are invited to join online test series.
+                </p>
+                <p>
+                  3. Registration for star performer contest started.
+                </p>
+                <p>
+                  4. Winners of last star performer contest are requested to collect their prize.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
