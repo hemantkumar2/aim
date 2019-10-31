9a258b41ebc79b083b4136bdab6b51a2c3ee5bfa
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-10-31 12:20:46 +0530

    home top magin reduced to 90px, about content font size reduced to 21px, in header vertical lines height reduce to 50px, ml-auto class added to navbar links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div>
       {/* <HeadNotification /> */}
-      <Navbar expand="lg" className='header-nav'>
+      <Navbar expand="lg" className='header-nav fixed-top'>
         <div className="row">
           <div className="col-3">
             <Link className='header-nav-brand' to='/'>
@@ -18,7 +18,7 @@ const Header = () => {
         <Navbar.Brand href="#home"><Link className='header-nav-brand mx-md-5' to='/'>AIM</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="mr-auto ml-auto">
             <Nav.Link><Link className='header-links mr-3  ml-5' to='/about'>About</Link></Nav.Link>
             <div className="vertical"></div>
             <Nav.Link><Link className='header-links mr-2 ml-2' to='/classroom'>Classroom</Link></Nav.Link>
--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -5,7 +5,7 @@ const Jumbotron = () => {
   return (
     <div>
       <div className=''>
-        <div className="jumbotron-fluid">
+        <div className="jumbotron-fluid mt-5">
           <div className="full-width">
             <img className='img-fluid m-0' src="assets/pic4.jpg" alt="" />
           </div>
