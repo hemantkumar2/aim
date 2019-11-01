b667e87957b4d375d4ab40acd92462de088c3b68
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-11-02 00:15:43 +0530

    margin and images added

--- src/Classroom.js
+++ src/Classroom.js
@@ -6,14 +6,14 @@ const Classroom = () => {
   return (
     <div>
       <Jumbotron />
-
       <h1 className='my-5'>CLASSROOM COURSES</h1>
       <div className="container my-2">
         <div className="row">
           <div className="col-md-4 mt-2 mt">
             <div className="card">
               <div className="bard-body">
-                <h4>School Tuition</h4>
+                <img src="assets/img5.svg" alt="" width='150px' />
+                <h4 className='mt-5 mb-3'>School Tuition</h4>
                 <p>We provide tuition classes for all classes from 6th to 12th.
                   All subjects for class 6th to 10th and PCB, PCM, and commerce for class 11th and 12th.
                   We have experienced group of teachers, who has long experience of teaching in the specific field.
@@ -24,7 +24,8 @@ const Classroom = () => {
           <div className="col-md-4 mt-2 mt">
             <div className="card">
               <div className="bard-body">
-                <h4>SSC/Railway/Vyapam/Bank</h4>
+                <img src="assets/img8.svg" alt="" width='250px' />
+                <h4 className='mt-5 mb-3'>SSC/Railway/Vyapam/Bank</h4>
                 <p>
                   We provide coaching classes for all government job aspirants like SSC,Railway,Bank, and CG Vyapam exams.
                     We have best faculty for Aptitude, Reasoning, English and General Studies.
@@ -36,7 +37,8 @@ const Classroom = () => {
           <div className="col-md-4 mt-2 mt">
             <div className="card">
               <div className="bard-body">
-                <h4>College</h4>
+                <img className='ml-5' src="assets/img1.svg" alt="" width='250px' />
+                <h4 className='mb-3'>College</h4>
                 <p>
                   We provide tuition classes for college student also.
                   All subjects for commerce and Arts stream. We have best faculty for the same who has broad knowledge of
