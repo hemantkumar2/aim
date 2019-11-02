c7d59caafffe7ad8c9afad4a7672a5b89b1daa58
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-11-02 11:51:53 +0530

    pictures added for experience teacher, personal attention, study-material and trust at why-us page

--- src/WhyUs.js
+++ src/WhyUs.js
@@ -8,72 +8,53 @@ const WhyUs = () => {
   return (
     <div>
       <Jumbotron />
-      <h1>WhyUs</h1>
-      <div className="row no-gutters offset-2 mt-4">
-        <div className="col-md-3 mr-5">
+      <h1 className='mb-3'>WhyUs</h1>
+      <div className="row  container-fluid no-gutters mt-4">
+        <div className="col-md-6">
           <div className="card">
             <div className="card-body">
-              <h4>Trust</h4>
-              <p className='text-muted'>
+              <h4 className='my-3'>Trust</h4>
+              <img src="assets/trust.svg" alt="" width='250px' />
+              <p className='text-muted mt-3'>
                 AIM Career Institute is one of the most trusted Government jobs coaching centers in Bilaspur.
                  Known for offering high quality career guidance to the aspirants.
               </p>
             </div>
           </div>
         </div>
-        <div className="col-md-3 mr-5">
+        <div className="col-md-6">
           <div className="card">
             <div className="card-body">
-              <h4>Experienced Faculty</h4>
-              <p className='text-muted'>
+              <h4 className='my-3'>Experienced Faculty</h4>
+              <img src="assets/experienced.svg" alt="" width='250px' />
+              <p className='text-muted mt-3'>
                 We have a team of Bilaspurs’s Experienced and Highly Qualified faculties who are ready to train and
                 help aspirants to achieve goals like clearing in minimum possible time.
               </p>
             </div>
           </div>
         </div>
-        <div className="col-md-3 mr-5">
+        <div className="col-md-6">
           <div className="card">
             <div className="card-body">
-              <h4>Personal Attention</h4>
-              <p className='text-muted'>
+              <h4 className='my-3'>Personal Attention</h4>
+              <img src="assets/personal.svg" alt="" width='250px' />
+              <p className='text-muted mt-3'>
                 Liberal hours of personal attention ensure that learners do not lag behind and stay on track.
                  To justify this we keep an optimum number students per batch so that personal attention can be given to students.
               </p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="row no-gutters offset-2 mt-4">
-        <div className="col-md-3 mr-5">
+
+        <div className="col-md-6">
           <div className="card">
             <div className="card-body">
-              <h4>Best Study Materials</h4>
-              <p className='text-muted'>
+              <h4 className='my-3'>Best Study Materials</h4>
+              <img src="assets/study-material.svg" alt="" width='250px' />
+              <p className='text-muted mt-3'>
                 Always Carefully chosen best study material provided to learners saves their time and keeps them focused.
                   We provide best study materials, so that aspirants can crack the exam with in minimal time duration.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3 mr-5">
-          <div className="card">
-            <div className="card-body">
-              <h4>Experienced Faculty</h4>
-              <p className='text-muted'>
-                We have a team of Bilaspur’s Experienced and Highly Qualified faculties who are ready to train and
-                help aspirants to achieve goals like clearing in minimum possible time.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3 mr-5">
-          <div className="card">
-            <div className="card-body">
-              <h4>Personal Attention</h4>
-              <p className='text-muted'>
-                Liberal hours of personal attention ensure that learners do not lag behind and stay on track.
-                 To justify this we keep an optimum number students per batch so that personal attention can be given to students.
               </p>
             </div>
           </div>
