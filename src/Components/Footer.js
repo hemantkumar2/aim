8a7cf4b4b35768610e913ff1b76abd8cf678e70f
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-11-02 16:30:43 +0530

    added a band at the bottom

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,27 +6,27 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div d-flex >
-      <div className="row footer wrap no-gutters mt-2 ">
+      <div className="row footer wrap no-gutters mt-1 ">
         <div className="col-12 ">
           <div className='container mt-3'>
             <a href="https://www.instagram.com" target='blank'>
-              <i className="text-white mr-3 fab fa-instagram fa-2x"></i>
+              <i className="text-white mr-3 fab fa-instagram fa-lg"></i>
             </a>
             <a href="https://www.facebook.com target='blank'">
-              <i className="text-white mr-3 fab fa-facebook fa-2x" ></i>
+              <i className="text-white mr-3 fab fa-facebook fa-lg" ></i>
             </a>
             <a href="https://www.youtube.com" target='blank'>
-              <i className="text-white fab mr-3 fa-youtube-square fa-2x"></i>
+              <i className="text-white fab mr-3 fa-youtube-square fa-lg"></i>
             </a>
             <a href="https://www.twitter.com" target='blank'>
-              <i class="fab text-white mr-3 fa-twitter-square fa-2x"></i>
+              <i class="fab text-white mr-3 fa-twitter-square fa-lg"></i>
             </a>
             <a href="https://www.linkedin.com target='blank'">
-              <i className="text-white fab mr-3 fa-linkedin fa-2x"></i>
+              <i className="text-white fab mr-3 fa-linkedin fa-lg"></i>
             </a>
           </div>
           <div>
-            <div className="container mt-3">
+            <div className="container mt-2">
               <Link to='/about' className='mr-3 footer-content'>About Us</Link>
               <Link to='/why-us' className='mr-3 footer-content'>Why Us</Link>
               {/* <Link to='/terms' className='mr-3 footer-content'>Terms and Conditions</Link> */}
@@ -34,7 +34,11 @@ const Footer = () => {
               {/* <Link to='/privacy' className='mr-3 footer-content'>Privacy Policy</Link> */}
               {/* <Link to='/faqs' className='mr-3 footer-content'>FAQ's</Link> */}
             </div>
-            <p className='footer-content my-2'>&copy; 2019 Aim Career Institute</p>
+            <div className="row bottom-band">
+              <div className="col-12 ">
+                <p className='footer-content my-2'>&copy; 2019 Aim Career Institute</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
